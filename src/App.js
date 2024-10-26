7e85ad69e85066af09a2f09c11d0de8da027d7b8
Nick <n.battista180@gmail.com>
2024-10-26 13:02:27 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nick & Kitty's Definitive Guide to Boulder, CO</h1>
+        <p>Welcome! We're so excited to show you where we live. Have a look at some of our favorite things to do here :)</p>
+        <iframe title="map" src="https://www.google.com/maps/d/embed?mid=1Y_Il1S6_7BPXw0Rqt9EkcdiBvLVEnl8&ehbc=2E312F" width="640" height="480"></iframe>      </header>
+    </div>
+  );
+}
+
+export default App;
